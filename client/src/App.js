aa29b059b28486593e6163c3ce1d7f4f448a1702
Joana Carballo <joana16cruz@gmail.com>
2022-03-03 17:50:35 +0100

    react-route: all plants

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import Plants from './pages/Plants';
 
 
 
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/plants' element={<Plants />} />
         </Routes>
       </header>
     </div>
---
+++ client/src/pages/Plants.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+
+
+export default function Plants() {
+
+    const [ plants, setPlants ] = useState([])
+    console.log("plants here: ", plants)
+
+    const storedToken = localStorage.getItem('authToken')
+
+    // get all the plants from the backend / server
+    const getAllPlants = () => {
+        // request 'api/plants'
+        // for every request to a project route we need to also send the token
+        axios.get('/plants', { headers: { Authorization: `Bearer ${storedToken}` } })
+            .then(response => {
+                //set the state of plants
+                setPlants(response.data)
+            })
+            .catch(err => {
+                console.log("error: ", err)
+            })
+    }
+
+    useEffect(() => {
+        getAllPlants()
+    }, [])
+
+	return (
+        <>
+            <h1>Here you can see all the plants</h1>
+            <div className="allPlants">
+               {plants.map((plant, i) => (
+                    <ul key={i} className="plant">
+                        <div>Common Name: {plant.common_name}</div>
+                        <div>Scientific Name: {plant.scientific_name}</div>
+                        <div>Description: {plant.description}</div>
+                    </ul>
+               ))}
+            </div>
+        </>
+		
+	)
+}
