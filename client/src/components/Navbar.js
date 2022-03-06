9091b4d1ab1cd9bbb163fdf1ee7adc428044712b
Joana Carballo <joana16cruz@gmail.com>
2022-03-06 14:23:49 +0100

    initial card

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
                 <img src="/home-icon.jpeg" alt="" width="24" height="24" className="d-inline-block align-text-top me-2" />
                 Planty
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
                     <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
                     <Link className="nav-link" to='/plants'>Plants</Link>
                 </div>
@@ -29,15 +29,6 @@ export default function Navbar() {
     )
 
 }
-
-{/* <nav className='navbar'>
-            <img className="nav-logo" src="../homepage.png" alt="img from logo" /> 
-            //<div>PLANTY</div>
-            //<Link className="nav-home" to='/'>Home</Link>
-           // <Link className="nav-plants" to='/plants'>Plants</Link>
-           // <Link className="nav-login" to='/login'>Login</Link>
-           // <Link className="nav-signup" to='/signup'>Signup</Link>
-       // </nav> */}
 
 
 
--- client/src/pages/Plants.js
+++ client/src/pages/Plants.js
@@ -1,59 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { useQueryParam, StringParam } from 'use-query-params';
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useQueryParam, StringParam } from "use-query-params";
 
 export default function Plants() {
-    
-    const [query, setQuery] = useQueryParam('query', StringParam)
-    console.log("query: ", query)
+  const [query, setQuery] = useQueryParam("query", StringParam);
+  console.log("query: ", query);
 
+  const [plants, setPlants] = useState([]);
+  console.log("plants here: ", plants);
 
-    const [ plants, setPlants ] = useState([])
-    console.log("plants here: ", plants)
-
-
-	// verifying if plant includes query
-    let filteredPlants = plants.filter(plant => {
-        return plant.common_name.toLowerCase().includes(query)
-    })
-
-
-
-    const storedToken = localStorage.getItem('authToken')
-
-    // get all the plants from the backend / server
-    const getAllPlants = () => {
-        // request 'api/plants'
-        // for every request to a project route we need to also send the token
-        axios.get('/plants', { headers: { Authorization: `Bearer ${storedToken}` } })
-            .then(response => {
-                //set the state of plants
-                setPlants(response.data)
-            })
-            .catch(err => {
-                console.log("error: ", err)
-            })
+  // verifying if plant includes query
+  let filteredPlants = plants.filter((plant) => {
+    if (query === undefined) {
+      return true;
     }
+    return plant.common_name.toLowerCase().includes(query);
+  });
+  console.log("filtered plants: ", filteredPlants);
+  const storedToken = localStorage.getItem("authToken");
 
-    useEffect(() => {
-        getAllPlants()
-    }, [])
+  // get all the plants from the backend / server
+  const getAllPlants = () => {
+    // request 'api/plants'
+    // for every request to a project route we need to also send the token
+    axios
+      .get("/plants", { headers: { Authorization: `Bearer ${storedToken}` } })
+      .then((response) => {
+        //set the state of plants
+        setPlants(response.data);
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+      });
+  };
 
-	return (
-        <>
-            <h1>Here you can see all the plants</h1>
-            <div className="allPlants">
-               {filteredPlants.map((plant, i) => (
-                    <ul key={i} className="plant">
-                        <div>Common Name: {plant.common_name}</div>
-                        <Link to={`/plants/${plant._id}`}>Details</Link>
-                    </ul>
-               ))}
+  useEffect(() => {
+    getAllPlants();
+  }, []);
+
+  return (
+    <>
+      <h1>Here you can see all the plants</h1>
+      <ul>
+        {filteredPlants.map((plant, i) => (
+          <li>
+            <div className="card" style={{background: '#fac400'}}>
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body" style={{background: 'white'}}>
+                <h5 className="card-title">{plant.common_name}</h5>
+                <p className="card-text">
+                  {plant.description}
+                </p>
+              </div>
             </div>
-        </>
-		
-	)
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+{
+  /* <div className="allPlants">
+  {filteredPlants.map((plant, i) => (
+    <ul key={i} className="plant">
+      <div>Common Name: {plant.common_name}</div>
+      <Link to={`/plants/${plant._id}`}>Details</Link>
+    </ul>
+  ))}
+</div>; */
 }
