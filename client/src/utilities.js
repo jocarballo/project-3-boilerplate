ed8e9701a7643a170ba2fb4fe9db41865931b370
Joana Carballo <joana16cruz@gmail.com>
2022-03-11 09:31:26 +0100

    refresh notes

--- client/src/utilities.js
+++ client/src/utilities.js
@@ -6,6 +6,16 @@ export default function getColorByIndex(index) {
     return colors[index % colors.length]
 } 
 
+// 0 => 0/5 => 0
+// 1 => 1/5 => 1
+// 2 => 2/5 => 2
+// ..
+// 5 => 5/5 => 0
+// 6 => 6/5 => 1
+// 7 => 7/5 => 2
+
+
+
 /* Identifiers for nav tabs */
 export const HOME_TAB = "home_tab"
 export const PLANTS_TAB = "plants_tab"
